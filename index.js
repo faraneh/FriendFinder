8f38f52627fe01dd57e48a8890a3b60929625f8d
faraneh <(FARane68)>
2021-10-28 12:43:41 -0400

    update2

---
+++ index.js
@@ -0,0 +1,23 @@
+//Varients
+const hamburgerVisual = document.querySelector('.hamburgerVisual');
+const ham = document.querySelectorAll('.ham');
+const ham1 = document.querySelector('.ham1');
+const ham2 = document.querySelector('.ham2');
+const ham3 = document.querySelector('.ham3');
+
+
+
+//Event Listeners
+hamburgerVisual.addEventListener('click', {
+    hamburgerandler()
+})
+
+
+
+
+//Functions
+
+hamburgerandler = (e) => { 
+    e.preventDefault();
+    console.log('Hi');
+}
